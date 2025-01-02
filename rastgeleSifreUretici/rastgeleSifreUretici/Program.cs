94112567d7b34d1c87fcae7c933aff20492399c3
ecemyaldiz02 <ecemyaldiz02@gmail.com>
2025-01-03 00:58:52 +0300

    Rastgele Şifre Belirleme
    Kullanıcının belirleyeceği kriterlere göre bir şifre oluşturur

---
+++ rastgeleSifreUretici/rastgeleSifreUretici/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Şifre Uzunluğunu Girin (8-15):");
+        int length = int.Parse(Console.ReadLine());
+
+        if (length < 8 || length > 15)
+        {
+            Console.WriteLine("Geçerli bir uzunluk girin! (8 ile 15 arasından bir uzunluk giriniz.)");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine("Büyük Harf Kullanılsın mı? (E/H):");
+        bool useUppercase = Console.ReadLine().ToUpper() == "E";
+
+        Console.WriteLine("Küçük Harf Kullanılsın mı? (E/H):");
+        bool useLowercase = Console.ReadLine().ToUpper() == "E";
+
+        Console.WriteLine("Rakam Kullanılsın mı? (E/H):");
+        bool useNumbers = Console.ReadLine().ToUpper() == "E";
+
+        Console.WriteLine("Özel Karakter Kullanılsın mı? (E/H):");
+        bool useSpecialChars = Console.ReadLine().ToUpper() == "E";
+
+        
+        string password = GeneratePassword(length, useUppercase, useLowercase, useNumbers, useSpecialChars);
+
+        Console.WriteLine($"Oluşturulan Şifre: {password}");
+
+        Console.ReadKey();
+    }
+
+    static string GeneratePassword(int length, bool useUppercase, bool useLowercase, bool useNumbers, bool useSpecialChars)
+    {
+        string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        string numbers = "0123456789";
+        string specialChars = "!@#$%^&*()_+";
+
+        StringBuilder characterPool = new StringBuilder();
+        if (useUppercase) characterPool.Append(uppercase);
+        if (useLowercase) characterPool.Append(lowercase);
+        if (useNumbers) characterPool.Append(numbers);
+        if (useSpecialChars) characterPool.Append(specialChars);
+
+        if (characterPool.Length == 0)
+        {
+            return "Geçerli bir karakter türü seçmediniz!";
+        }
+
+        Random random = new Random();
+        StringBuilder password = new StringBuilder();
+
+        
+        for (int i = 0; i < length; i++)
+        {
+            int index = random.Next(characterPool.Length);  
+            password.Append(characterPool[index]);  
+        }
+
+        return password.ToString();  
+    }
+}
---
+++ rastgeleSifreUretici/rastgeleSifreUretici/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rastgeleSifreUretici")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rastgeleSifreUretici")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4ed9a0ae-ca90-4441-8947-7f40f7d7d9a9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
